c36f3616114d6cda84edfc90ff38fdc89feb7bcc
Paolo Tacconi <p.tacconi@giunti.it>
2020-12-09 12:48:10 +0100

    Initialized with start.spring.io

---
+++ src/test/java/it/tarine/hurricane/HurricaneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.tarine.hurricane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurricaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
